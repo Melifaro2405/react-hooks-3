7b86700271795ff63b17cdac18a9edac8bdd54f4
Denis <tomelifaro@yandex.ru>
2021-01-25 23:52:58 +0300

    github search with hooks without redux

---
+++ src/components/Alert.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react';
+import {AlertContext} from "../context/alert/alertContext";
+
+export const Alert = () => {
+  const {alert, hide} = useContext(AlertContext);
+
+  if(!alert) return null;
+  const {type, text} = alert;
+
+  return (
+    <div
+      className={`alert alert-${type || 'secondary'} alert-dismissible fade show`}
+      role="alert"
+    >
+      {text}
+      <button type="button" className="close" aria-label="Close" onClick={hide}>
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export const Card = ({user}) => {
+  const {avatar_url, login} = user;
+
+  return (
+    <div className="card">
+      <img src={avatar_url} alt={login} className="card-img-top"/>
+      <div className="card-body">
+        <h5 className="card-title">{login}</h5>
+        <Link to={`/profile/${login}`} className="btn btn-primary">Открыть</Link>
+      </div>
+    </div>
+  );
+};
